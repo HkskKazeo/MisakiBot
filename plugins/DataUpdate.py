e8b36b9b92fd6b8517d041485e032dbddb379f96
HkskKazeo <594246389@qq.com>
2019-09-10 00:15:17 +0800

    历史数据

--- plugins/DataUpdate.py
+++ plugins/DataUpdate.py
@@ -9,45 +9,74 @@ from datetime import datetime
 async def _():
     async with aiohttp.request('GET', 'https://api.matsurihi.me/mltd/v1/events') as resp:
         json = await resp.json()
-        time = datetime.strptime(json[-1]['schedule']['endDate'], "%Y-%m-%dT%H:%M:%S+09:00")
-        if (json[-1]['type'] == 3 or json[-1]['type'] == 4) and time > datetime.now():
-            sqlconn = sqlite3.connect('C:\CQ\酷Q Pro\MltdBot.db')
-            cursor = sqlconn.cursor()
-            for i in (1, 2, 3, 10, 100, 2500, 5000, 10000, 25000, 50000):
-                str_rank = 'https://api.matsurihi.me/mltd/v1/events/' + str(json[-1]['id']) \
-                           + '/rankings/logs/eventPoint/' + str(i)
-                async with aiohttp.request('GET', str_rank) as resp_rank:
-                    json_rank = await resp_rank.json()
-                    for j in reversed(json_rank[0]['data']):
-                        timestamp = datetime.strptime(j["summaryTime"], "%Y-%m-%dT%H:%M:%S+09:00")
-                        result = cursor.execute("SELECT * FROM EventHistory where EventID = ? AND Rank = ? AND Time = ?",
-                                               (json[-1]['id'], i, timestamp))
-                        if len(list(result)) == 0:
-                            timestart = datetime.strptime(json_rank[0]["data"][0]["summaryTime"],
-                                                                   "%Y-%m-%dT%H:%M:%S+09:00")
-                            timestart = datetime(timestart.year, timestart.month, timestart.day, 15, 0, 0)
-                            hours = (timestamp - timestart).days * 24.0 + (timestamp - timestart).seconds / 3600.0
-                            if json_rank[0]["data"].__len__ == 1:
-                                diff = j["score"]
-                            else:
-                                diff = j["score"] - json_rank[0]["data"][json_rank[0]["data"].index(j)-1]["score"]
-                            cursor.execute("INSERT INTO EventHistory (EventID, Rank, Time, HoursAfterBegin, \
-                            EventPT, PTIncrease) VALUES(?, ?, ?, ?, ?, ?)", (json[-1]['id'], i, timestamp,
-                                                                             hours, j["score"], diff))
-                            sqlconn.commit()
-                        else:
-                            break
-            timelast = datetime.strptime(json_rank[0]["data"][-1]["summaryTime"], "%Y-%m-%dT%H:%M:%S+09:00")
-            result = cursor.execute("SELECT * FROM EventHistory where EventID = ? AND Time = ? ORDER BY Rank",
-                           (json[-1]['id'], timelast))
-            values = result.fetchall()
-            if len(values) > 0:
-                global str_ptnew
-                str_ptnew = '活动名称: ' + json[-1]['name'] + '\n当前时间: ' + str(values[0][2]) +\
-                      '\n已经过: ' + str(values[0][3]) + '小时\n========\n'
-                for j in values:
-                    str_ptnew += str(j[1]) + ':\t' + str(j[4]) + '(+' + str(j[5]) + ')\n'
-                bot = nonebot.get_bot()
-                await bot.send_private_msg(user_id=594246389, message=str_ptnew)
+    time = datetime.strptime(json[-1]['schedule']['endDate'], "%Y-%m-%dT%H:%M:%S+09:00")
+    if (json[-1]['type'] == 3 or json[-1]['type'] == 4) and time > datetime.now():
+        sqlconn = sqlite3.connect('MltdBot.db')
+        cursor = sqlconn.cursor()
+        check_eventinfo(sqlconn, cursor, json[-1])
+        for i in (1, 2, 3, 10, 100, 2500, 5000, 10000, 25000, 50000):
+            str_rank = 'https://api.matsurihi.me/mltd/v1/events/' + str(json[-1]['id']) \
+                       + '/rankings/logs/eventPoint/' + str(i)
+            async with aiohttp.request('GET', str_rank) as resp_rank:
+                json_rank = await resp_rank.json()
+            dbwrite_event(sqlconn, json[-1]['id'], i, json_rank)
+
+        timelast = datetime.strptime(json_rank[0]["data"][-1]["summaryTime"], "%Y-%m-%dT%H:%M:%S+09:00")
+
+        result = cursor.execute("SELECT * FROM EventHistory where EventID = ? AND Time = ? ORDER BY Rank",
+                                (json[-1]['id'], timelast))
+        values = result.fetchall()
+        generate_gbstr(values, json[-1]['name'])
+
+    else:
+        global str_ptnew, str_hsnew
+        str_ptnew = '当前不在活动时段内，请使用历史档线查询功能'
+        str_hsnew = '当前不在活动时段内，请使用历史档线查询功能'
+
+
+def check_eventinfo(sqlconn, sqlcursor, json):
+        time_s = datetime.strptime(json["schedule"]["beginDate"], "%Y-%m-%dT%H:%M:%S+09:00")
+        time_e = datetime.strptime(json["schedule"]["endDate"], "%Y-%m-%dT%H:%M:%S+09:00")
+        time_b = datetime.strptime(json["schedule"]["boostBeginDate"], "%Y-%m-%dT%H:%M:%S+09:00")
+        length = (time_e - time_s).days * 24.0 + (time_e - time_s).seconds / 3600.0
+        boostlength = (time_e - time_b).days * 24.0 + (time_e - time_b).seconds / 3600.0
+        sqlcursor.execute("INSERT INTO EventInfo (ID, Name, Type, BeginTime, EndTime, Length, BoostStart, \
+            BoostLength) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (json["id"], json["name"],
+                                                     json["type"], time_s, time_e, length, time_b,
+                                                     boostlength))
+        sqlconn.commit()
+
+
+def dbwrite_event(sqlconn, id, rank, json_rank):
+    for j in reversed(json_rank[0]['data']):
+        cursor = sqlconn.cursor()
+        timestamp = datetime.strptime(j["summaryTime"], "%Y-%m-%dT%H:%M:%S+09:00")
+        result = cursor.execute(
+            "SELECT * FROM EventHistory where EventID = ? AND Rank = ? AND Time = ?", (id, rank, timestamp))
+        if len(list(result)) == 0:
+            timestart = datetime.strptime(json_rank[0]["data"][0]["summaryTime"],
+                                          "%Y-%m-%dT%H:%M:%S+09:00")
+            timestart = datetime(timestart.year, timestart.month, timestart.day, 15, 0, 0)
+            hours = (timestamp - timestart).days * 24.0 + (timestamp - timestart).seconds / 3600.0
+            if json_rank[0]["data"].__len__ == 1:
+                diff = j["score"]
+            else:
+                diff = j["score"] - json_rank[0]["data"][json_rank[0]["data"].index(j) - 1]["score"]
+            cursor.execute("INSERT INTO EventHistory (EventID, Rank, Time, HoursAfterBegin, \
+            EventPT, PTIncrease) VALUES(?, ?, ?, ?, ?, ?)", (id, rank, timestamp,
+                                                             hours, j["score"], diff))
+            sqlconn.commit()
+        else:
+            break
+
+def generate_gbstr(values, name):
+    if len(values) > 0:
+        global str_ptnew
+        str_ptnew = '活动名称: ' + name + '\n当前时间: ' + str(values[0][2]) + \
+                    '\n已经过: ' + str(values[0][3]) + '小时\n========\n'
+        for j in values:
+            str_ptnew += str(j[1]) + ':\t' + str(j[4]) + '(+' + str(j[5]) + ')\n'
+
+
 
 
--- plugins/test.py
+++ plugins/test.py
@@ -1,11 +1,74 @@
 import nonebot
 from nonebot import on_command, CommandSession
+from datetime import datetime
+import sqlite3
+import aiohttp
+import urllib
+from urllib import request
+import json
 
 
 @on_command('test')
 async def _(session: CommandSession):
     bot = nonebot.get_bot()
     await bot.send_private_msg(user_id=594246389, message='test')
+
+    sqlconn = sqlite3.connect('MltdBot.db')
+    sqlcursor = sqlconn.cursor()
+
+    str_catalog = 'https://api.matsurihi.me/mltd/v1/events' #活动列表
+    req = urllib.request.Request(str_catalog)
+    try:
+        response = request.urlopen(str_catalog)
+    except:
+        exit()
+    html = response.read()
+    json_catalog = json.loads(html)
+    for i in range(len(json_catalog)):
+        if json_catalog[i]['id'] > 32 and (json_catalog[i]["type"] == 3 or json_catalog[i]["type"] == 4): #扩线后的传统/巡演
+            time_s = datetime.strptime(json_catalog[i]["schedule"]["beginDate"], "%Y-%m-%dT%H:%M:%S+09:00")
+            time_e = datetime.strptime(json_catalog[i]["schedule"]["endDate"], "%Y-%m-%dT%H:%M:%S+09:00")
+            time_b = datetime.strptime(json_catalog[i]["schedule"]["boostBeginDate"], "%Y-%m-%dT%H:%M:%S+09:00")
+            length = (time_e - time_s).days * 24.0 + ((time_e - time_s).seconds + 1)/ 3600.0
+            boostlength = (time_e - time_b).days * 24.0 + ((time_e - time_b).seconds + 1) / 3600.0
+            sqlcursor.execute("INSERT INTO EventInfo (ID, Name, Type, BeginTime, EndTime, Length, BoostStart, \
+                BoostLength) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (json_catalog[i]["id"], json_catalog[i]["name"],
+                              json_catalog[i]["type"], time_s, time_e, length, time_b, boostlength))
+            sqlconn.commit()
+            for j in (1, 2, 3, 10, 100, 2500, 5000, 10000, 25000, 50000):
+                read_rankpage(sqlconn, sqlcursor, json_catalog[i]["id"], j)
+
+
+def read_rankpage(sqlconn, sqlcursor, event, rank):
+    str_page = 'https://api.matsurihi.me/mltd/v1/events/' + str(event) + '/rankings/logs/eventPoint/' + str(rank)
+    req = urllib.request.Request(str_page)
+    try:
+        response = request.urlopen(str_page)
+    except:
+        print('error' + str(event) + '_' + str(rank))
+        return
+    html = response.read()
+    json_rank = json.loads(html)
+    try:
+        timestart = datetime.strptime(json_rank[0]["data"][0]["summaryTime"], "%Y-%m-%dT%H:%M:%S+09:00")
+    except:
+        return;
+    #第一条记录的时间并非开始时间
+    timestart = datetime(timestart.year, timestart.month, timestart.day, 15, 0, 0)
+    for i in range(len(json_rank[0]["data"])):
+        timenow = datetime.strptime(json_rank[0]["data"][i]["summaryTime"], "%Y-%m-%dT%H:%M:%S+09:00")
+        hours = (timenow - timestart).days * 24.0 + (timenow - timestart).seconds / 3600.0
+        score = json_rank[0]["data"][i]["score"]
+        if(i == 0):
+            diff = score
+        else:
+            diff = score - json_rank[0]["data"][i - 1]["score"]
+        sqlcursor.execute("INSERT INTO EventHistory (EventID, Rank, Time, HoursAfterBegin, EventPT, PTIncrease) \
+                          VALUES(?, ?, ?, ?, ?, ?)", (event, rank, timenow, hours, score, diff))
+    sqlconn.commit()
+
+
+
 
 
 
